dfe6c0271ad6c2be34adb5f804a92bbc50ab5f98
Hexexe <TusernameAken@gmail.com>
2020-04-07 04:08:31 +0300

    ristinolla update

--- src/Tehtävä_8_TemplateMethod/TicTacToe.java
+++ src/Tehtävä_8_TemplateMethod/TicTacToe.java
@@ -1,20 +1,21 @@
 package Tehtävä_8_TemplateMethod;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class TicTacToe extends Game {
 
-    private String[][] board = new String[3][3];
     public static Scanner s = new Scanner(System.in);
+    private Map<Integer, Object> map = new HashMap<>();
     private int currentPlayer;
+    private boolean validMove;
+    private String ss;
 
     @Override
     void initializeGame() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = "_";
-            }
-        }
+        for (int i = 1; i < 10; i++) { map.put(i, "_"); }
         printBoard();
     }
 
@@ -22,18 +23,24 @@ public class TicTacToe extends Game {
     void makePlay(int player) {
         currentPlayer = player;
         System.out.printf("\nPlayer %d Choose coordinates (1-9): ", player + 1);
-        while (!s.hasNext("[1-9]")) {
-            System.out.println("Try again");
-            s.next();
+        validMove = false;
+        while (!validMove) {
+            try {
+                ss = s.next();
+                while (!(map.get(Integer.parseInt(ss)).equals("_") && ss.matches("[1-9]"))) {
+                    System.out.println("Invalid Input");
+                    ss = s.next();
+                }
+                checkInput(Integer.parseInt(ss), player);
+                validMove = true;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
-        checkInput(s.nextInt(), player);
     }
-
 
     @Override
-    boolean endOfGame() {
-        return checkWinner();
-    }
+    boolean endOfGame() { return checkWinner(); }
 
     @Override
     void printWinner() {
@@ -41,63 +48,31 @@ public class TicTacToe extends Game {
     }
 
     public void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            System.out.println();
-            for (int j = 0; j < 3; j++) {
-                if (j == 0) System.out.print(" | ");
-                System.out.print(board[i][j] + " | ");
-            }
-        }
+        map.entrySet().forEach(e -> {
+            System.out.print(" | " + e.getValue());
+            if (IntStream.of(3, 6, 9).anyMatch(i -> i == e.getKey())) System.out.println(" | ");
+        });
     }
 
-
     public void checkInput(int i, int player) {
-
         String s = (player == 0) ? "X" : "O";
-
-        switch (i) {
-            case 1:
-                board[0][0] = s;
-                break;
-            case 2:
-                board[0][1] = s;
-                break;
-            case 3:
-                board[0][2] = s;
-                break;
-            case 4:
-                board[1][0] = s;
-                break;
-            case 5:
-                board[1][1] = s;
-                break;
-            case 6:
-                board[1][2] = s;
-                break;
-            case 7:
-                board[2][0] = s;
-                break;
-            case 8:
-                board[2][1] = s;
-                break;
-            case 9:
-                board[2][2] = s;
-                break;
-            default:
-                break;
-        }
+        map.replace(i, s);
         printBoard();
     }
 
     private boolean checkWinner() {
-        //rivi
-        for (int i = 0; i < 3; i++) { if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != "_") return true; }
-        //sarake
-        for (int j = 0; j < 3; j++) { if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != "_") return true; }
+        // kovakoodattu väliaikasesti
         // vaaka
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != "_") return true;
-        // full
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != "_") return true;
+        if (map.get(1) == map.get(2) && map.get(2) == map.get(3) && map.get(1) != "_") return true;
+        if (map.get(4) == map.get(5) && map.get(5) == map.get(6) && map.get(4) != "_") return true;
+        if (map.get(7) == map.get(8) && map.get(8) == map.get(9) && map.get(7) != "_") return true;
+        // pysty
+        if (map.get(1) == map.get(4) && map.get(4) == map.get(7) && map.get(1) != "_") return true;
+        if (map.get(2) == map.get(5) && map.get(5) == map.get(8) && map.get(2) != "_") return true;
+        if (map.get(3) == map.get(6) && map.get(6) == map.get(9) && map.get(3) != "_") return true;
+        // sivu
+        if (map.get(1) == map.get(5) && map.get(5) == map.get(9) && map.get(1) != "_") return true;
+        if (map.get(3) == map.get(5) && map.get(5) == map.get(7) && map.get(3) != "_") return true;
 
         return false;
     }
