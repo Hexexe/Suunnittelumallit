91c6d86e95d9eb88663119329c075a0292a92759
Hexexe <TusernameAken@gmail.com>
2020-04-15 09:20:30 +0300

    immutable -> mutable

--- src/Tehtävä_12_Proxy/Main.java
+++ src/Tehtävä_12_Proxy/Main.java
@@ -1,12 +1,13 @@
 package Tehtävä_12_Proxy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Image> album = new ArrayList<>(List.of(new ProxyImage("AmphibianHateSymbol.png"), new ProxyImage("Cheerful_Trader.gif")));
+        List<Image> album = Arrays.asList(new ProxyImage("AmphibianHateSymbol.png"), new ProxyImage("Cheerful_Trader.gif"));
         album.forEach(Image::showData);
         album.forEach(Image::displayImage);
     }
