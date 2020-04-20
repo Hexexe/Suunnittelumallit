fb30d7a4db0a53cae578080089624d5ca9de83d0
Hexexe <TusernameAken@gmail.com>
2020-04-20 15:55:51 +0300

    rip

--- src/Tehtävä_15_Adapter/Main.java
+++ src/Tehtävä_15_Adapter/Main.java
@@ -3,7 +3,7 @@ package Tehtävä_15_Adapter;
 public class Main {
 
     public static void main(String[] args) {
-        BinaryAdapter c = new BinaryAdapater_C(new Luku(13501488));
+        BinaryAdapter c = new BinaryAdapter_C(new Luku(13501488));
         System.out.println(c.convert());
     }
 }
