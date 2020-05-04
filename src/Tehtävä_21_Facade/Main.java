5135d2beb1b966bf9ebac3a5c5821f7a1947dbaa
Hexexe <TusernameAken@gmail.com>
2020-05-04 19:58:00 +0300

    TempleOS

---
+++ src/Tehtävä_21_Facade/Main.java
@@ -0,0 +1,8 @@
+package Tehtävä_21_Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputerFacade computer = new ComputerFacade();
+        computer.start();
+    }
+}
