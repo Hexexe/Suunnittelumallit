a8ef7eda3c45bb2b3f3b6ea51cf059c65e99eae9
Hexexe <TusernameAken@gmail.com>
2020-05-05 02:38:22 +0300

    Iterator

---
+++ src/Tehtävä_20_Iterator/ThreadN.java
@@ -0,0 +1,22 @@
+package Tehtävä_20_Iterator;
+
+import java.util.Iterator;
+
+public class ThreadN extends Thread {
+
+    private Iterator<Integer> i;
+    private String name;
+
+    public ThreadN(Iterator<Integer> i, String name) {
+        this.i = i;
+        this.name = name;
+    }
+
+    public void run() {
+        while (i.hasNext()) {
+            Integer v = i.next();
+            if (name.equals("Purge") && v % 2 == 0) i.remove(); // delete test
+            else System.out.printf("%s : %d\n", name, v);
+        }
+    }
+}
